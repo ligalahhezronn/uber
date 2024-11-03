7a75bbef4edefb9e8d8b2bfebf699736bbba5435
ligalahhezronn <ligalahh@gmail.com>
2024-11-03 01:41:57 -0800

    day two

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   content: [
-      "./App.{js,jsx,ts,tsx}",
-      "./screens/**/*.{js,jsx,ts,tsx}",
+      "./app.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
